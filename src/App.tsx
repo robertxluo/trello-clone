d28d91ec24affb5c5aa66dfbd9a5c71f4a1e8d62
Robert Luo <robertxluo@gmail.com>
2022-05-06 12:56:25 -0700

    Prepare styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
-
+import { AppContainer, CardContainer, ColumnContainer, ColumnTitle } from './styles';
 
 export function App() {
-  return <>Hello</>
+  return (
+    <AppContainer>
+      <ColumnContainer>
+        <ColumnTitle>Todo:</ColumnTitle>
+        <CardContainer>First Item</CardContainer>
+        <CardContainer>Second Item</CardContainer>
+        <CardContainer>Third Item</CardContainer>
+      </ColumnContainer>
+    </AppContainer>
+  );
 }
-
