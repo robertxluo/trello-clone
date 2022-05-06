020aa8cb104c9418064c0f1f0e8cd65a6dc85ac1
Robert Luo <robertxluo@gmail.com>
2022-05-06 16:47:14 -0700

    NewItemForm component

--- src/AddNewItem.tsx
+++ src/AddNewItem.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { NewItemForm } from './NewItemForm';
 import { AddItemButton } from './styles';
 
 type AddNewItemProps = {
@@ -11,6 +12,14 @@ export function AddNewItem({ onAdd, toggleButtonText, dark }: AddNewItemProps) {
   const [showForm, setShowForm] = useState(false);
 
   if (showForm) {
+    return (
+      <NewItemForm
+        onAdd={(text) => {
+          onAdd(text);
+          setShowForm(false);
+        }}
+      />
+    );
   }
 
   return (
--- src/styles.ts
+++ src/styles.ts
@@ -54,3 +54,31 @@ export const AddItemButton = styled.button<AddItemButtonProps>`
     background-color: #ffffff52;
   }
 `;
+
+export const NewItemFormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  max-width: 300px;
+  width: 100%;
+`;
+
+export const NewItemButton = styled.button`
+  background-color: #5aac44;
+  border-radius: 3px;
+  border: none;
+  box-shadow: none;
+  color: #fff;
+  padding: 6px 12px;
+  text-align: center;
+  cursor: pointer;
+`;
+
+export const NewItemInput = styled.input`
+  border-radius: 3px;
+  border: none;
+  box-shadow: #091e4240 0px 1px 0px 0px;
+  margin-bottom: 0.5rem;
+  padding: 0.5rem 1rem;
+  width: 100%;
+`;
